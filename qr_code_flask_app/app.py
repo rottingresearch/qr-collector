f30c1bb0c3d1431575177d26182e7264cf1f1ee7
Marshal Miller <30907840+marshalmiller@users.noreply.github.com>
2024-10-13 20:57:50 -0400

    Fixed functionality.

--- qr_code_flask_app/app.py
+++ qr_code_flask_app/app.py
@@ -20,8 +20,12 @@ with app.app_context():
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/stored_qr_codes')
+def stored_qr_codes():
     qr_codes = QRCode.query.all()
-    return render_template('index.html', qr_codes=qr_codes)
+    return render_template('stored_qr_codes.html', qr_codes=qr_codes)
 
 @app.route('/scan', methods=['POST'])
 def scan_qr_code():
@@ -35,10 +39,6 @@ def scan_qr_code():
     # Process the uploaded file (existing functionality)
     # ...
 
-@app.route('/scan_camera')
-def scan_camera():
-    return render_template('scan_camera.html')
-
 @app.route('/process_qr_code')
 def process_qr_code():
     data = request.args.get('data')
@@ -46,7 +46,7 @@ def process_qr_code():
         new_qr_code = QRCode(data=data)
         db.session.add(new_qr_code)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('stored_qr_codes'))
     return 'No QR code data found', 400
 
 if __name__ == '__main__':
